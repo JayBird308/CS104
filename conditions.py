56f7a383e0cdbbecd1c4cf39843b4a5c495afa8e
JayBird308 <60233269+JayBird308@users.noreply.github.com>
2020-02-12 09:41:17 -0500

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -2,7 +2,7 @@
 temp = input("Please enter the current temperature (°F): ")
 #Convert the input string temperature to an integer
 temp = int(temp)
-#Set up the equality for the input temperature
+#Set up the equality statement for the input temperature
 if temp < 70:
     print ("Wear a jacket")
 else:
