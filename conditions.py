2c2cd59ed9522351403403a4b46e36b314b6a68e
JayBird308 <60233269+JayBird308@users.noreply.github.com>
2020-02-12 09:37:00 -0500

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,9 @@
+#Ask the user to input the temperature
+temp = input("Please enter the current temperature: ")
+#Convert the input string temperature to an integer
+temp = int(temp)
+#Set up the equality for the input temperature
+if temp < 70:
+    print ("Wear a jacket")
+else:
+    print ("No jacket required")
